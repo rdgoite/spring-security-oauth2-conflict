aacd0c8e6aa6ececb818f8931433a70bb3e58715
rdgoite <rdgoite@gmail.com>
2016-05-14 13:06:30 +0800

    Added security policy to /books/secured.

--- src/main/groovy/me/psychopunch/spring/demo/security/BookController.groovy
+++ src/main/groovy/me/psychopunch/spring/demo/security/BookController.groovy
@@ -1,5 +1,6 @@
 package me.psychopunch.spring.demo.security
 
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
@@ -14,6 +15,7 @@ class BookController {
     }
 
     @RequestMapping(path='secured', method=RequestMethod.GET)
+    @PreAuthorize('authenticated')
     def securedBooks() {
         return [new Book(title: 'Secured Book', author: 'John Doe')]
     }
--- src/main/groovy/me/psychopunch/spring/demo/security/SecurityConfiguration.groovy
+++ src/main/groovy/me/psychopunch/spring/demo/security/SecurityConfiguration.groovy
@@ -2,6 +2,7 @@ package me.psychopunch.spring.demo.security
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 class SecurityConfiguration {
 
     @Configuration
+    @EnableGlobalMethodSecurity(prePostEnabled=true)
     static class WebSecurity extends WebSecurityConfigurerAdapter {
 
         @Override
