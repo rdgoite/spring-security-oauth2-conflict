03b0c5a5f657003d30d36a1deda6b633b97f3670
rdgoite <rdgoite@gmail.com>
2016-05-14 12:44:23 +0800

    Created demo application to demonstrate issue with Spring security.

---
+++ src/main/groovy/me/psychopunch/spring/demo/security/SecurityConfiguration.groovy
@@ -0,0 +1,32 @@
+package me.psychopunch.spring.demo.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+class SecurityConfiguration {
+
+    @Configuration
+    static class WebSecurity extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication().withUser("user").password("password").authorities("ROLE_USER")
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .anyRequest()
+                    .permitAll()
+                    .and()
+                    .formLogin().defaultSuccessUrl("http://example.com")
+                    .and()
+                    .logout()
+        }
+
+    }
+
+}
---
+++ src/main/groovy/me/psychopunch/spring/demo/security/SpringSecurityConflictApplication.groovy
@@ -0,0 +1,12 @@
+package me.psychopunch.spring.demo.security
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringSecurityConflictApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SpringSecurityConflictApplication, args
+	}
+}
